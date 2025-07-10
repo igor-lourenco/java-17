93ed650a8481b49857ef12c4f5f868e345670b54
igor-lourenco <igor.lourencosantos@gmail.com>
2025-07-10 17:24:25 -0300

    Entendendo o polimorfismo

--- src/com/java17/_5_polimorfismoEClassesAbstratas/banco/CaixaEletronico.java
+++ src/com/java17/_5_polimorfismoEClassesAbstratas/banco/CaixaEletronico.java
@@ -5,7 +5,7 @@ public class CaixaEletronico {
 
     public static final double TARIFA_TRANSFERENCIA = 10.0;
 
-    public void transferir(ContaEspecial contaOrigem, ContaEspecial contaDestino, double valorTransferencia){
+    public void transferir(Conta contaOrigem, Conta contaDestino, double valorTransferencia){
 
         System.out.println();
         System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%d%n",
@@ -17,16 +17,5 @@ public class CaixaEletronico {
         contaDestino.depositar(valorTransferencia);
     }
 
-    public void transferir(ContaEspecial contaOrigem, ContaSalario contaDestino, double valorTransferencia){
-
-        System.out.println();
-        System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%d%n",
-                valorTransferencia, contaOrigem.getAgencia(), contaOrigem.getNumero(),
-                contaDestino.getAgencia(), contaDestino.getNumero());
-
-
-        contaOrigem.sacar(valorTransferencia + TARIFA_TRANSFERENCIA);
-        contaDestino.depositar(valorTransferencia);
-    }
 
 }
