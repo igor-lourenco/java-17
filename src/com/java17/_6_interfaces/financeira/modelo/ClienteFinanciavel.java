d20ea2a181a03580e81e8f5703a7ed390c4bffb8
igor-lourenco <igor.lourencosantos@gmail.com>
2025-07-18 10:40:55 -0300

    Variaveis são estáticas e finais em interfaces

--- src/com/java17/_6_interfaces/financeira/modelo/ClienteFinanciavel.java
+++ src/com/java17/_6_interfaces/financeira/modelo/ClienteFinanciavel.java
@@ -2,15 +2,19 @@ package com.java17._6_interfaces.financeira.modelo;
 
 public interface ClienteFinanciavel {
 
+    public static final double JUROS_BAIXO_RISCO = 1.0;
+    double JUROS_MEDIO_RISCO = 1.5;
+    double JUROS_ALTO_RISCO = 2.0;
+
     public abstract double calcularLimiteAprovado();
 
     default double calcularJuros(double valorSolicitado){
         if(isFinanciamentoPequenoValor(valorSolicitado)){
-            return 1.0;
+            return JUROS_BAIXO_RISCO;
         } else if (isFinanciamentoGrandeValor(valorSolicitado)) {
-            return 1.5;
+            return JUROS_MEDIO_RISCO;
         }
-        return 2.0;
+        return JUROS_ALTO_RISCO;
     }
 
     static boolean isFinanciamentoPequenoValor(double valorSolicitado) {
