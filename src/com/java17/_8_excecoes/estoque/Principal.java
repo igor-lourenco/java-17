01b567997d0cd468c04c296112ecf4268c876f77
igor-lourenco <igor.lourencosantos@gmail.com>
2025-07-22 11:41:25 -0300

    Stack Trace, Interpretando e analisando exceções

--- src/com/java17/_8_excecoes/estoque/Principal.java
+++ src/com/java17/_8_excecoes/estoque/Principal.java
@@ -2,6 +2,8 @@ package com.java17._8_excecoes.estoque;
 
 import com.java17._8_excecoes.estoque.modelo.Produto;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -11,8 +13,21 @@ public class Principal {
 
         produto.ativar();
 
-        produto.retirarEstoque(15);
+        comprar(produto);
+    }
 
-        System.out.printf("%nEstoque: %d%n", produto.getQuantidadeEstoque());
+    private static void comprar(Produto produto){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantidade: ");
+        int quantidade = scanner.nextInt();
+
+        efetuarBaixaEstoque(produto, quantidade);
+        System.out.println("Compra realizada!");
+    }
+
+    private static void efetuarBaixaEstoque(Produto produto, int quantidade) {
+        produto.retirarEstoque(quantidade);
+        System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n",
+                quantidade, produto.getQuantidadeEstoque());
     }
 }
