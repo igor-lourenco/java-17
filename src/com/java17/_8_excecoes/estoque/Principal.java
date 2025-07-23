1d2ef9c3dbf0b585eed3b61be0e0c4d9d306b79e
igor-lourenco <igor.lourencosantos@gmail.com>
2025-07-23 11:39:38 -0300

    Capturando e lançando nova exceção

--- src/com/java17/_8_excecoes/estoque/Principal.java
+++ src/com/java17/_8_excecoes/estoque/Principal.java
@@ -1,8 +1,7 @@
 package com.java17._8_excecoes.estoque;
 
+import com.java17._8_excecoes.estoque.exceptions.BaixaEstoqueException;
 import com.java17._8_excecoes.estoque.exceptions.ProdutoException;
-import com.java17._8_excecoes.estoque.exceptions.ProdutoInativoException;
-import com.java17._8_excecoes.estoque.exceptions.ProdutoSemEstoqueException;
 import com.java17._8_excecoes.estoque.modelo.Produto;
 
 import java.util.Scanner;
@@ -30,26 +29,8 @@ public class Principal {
                 System.out.println("Compra realizada!");
 
                 break;
-            } catch (IllegalArgumentException e) {
+            } catch (BaixaEstoqueException e) {
 //                e.printStackTrace(System.out); // Usando a saida padrão em vez da saida de erro
-                System.out.println("Erro na compra: " + e.getMessage());
-            } catch (ProdutoSemEstoqueException e){
-                System.out.printf("Erro na compra: %s . Estoque disponivel: %d. Estoque necessario: %d%n",
-                        e.getMessage(), e.getEstoqueDisponivel(), e.getEstoqueNecessario());
-            } catch (ProdutoInativoException e) {
-//                e.printStackTrace(System.out); // Usando a saida padrão em vez da saida de erro
-                System.out.println("Erro na compra: " + e.getMessage());
-                System.out.print("Deseja ativar o produto? ");
-
-                if(scanner.nextBoolean()){
-                    produto.ativar();
-                    System.out.println("Ok, produto ativado");
-                }else{
-                    System.out.println("Ok. Compra nao pode ser realizada");
-                    break;
-                }
-            }catch (ProdutoException e){
-                System.out.println(e.getClass().getName());
                 System.out.println("Erro na compra: " + e.getMessage());
             }
 
@@ -57,13 +38,16 @@ public class Principal {
 
     }
 
-    private static void efetuarBaixaEstoque(Produto produto, int quantidade) throws ProdutoSemEstoqueException, ProdutoInativoException {
-//        try {
+    private static void efetuarBaixaEstoque(Produto produto, int quantidade) throws BaixaEstoqueException {
+        try {
             produto.retirarEstoque(quantidade);
             System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n",quantidade, produto.getQuantidadeEstoque());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
-//            throw e;
-//        }
+        } catch (IllegalArgumentException e){
+            System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
+            throw new BaixaEstoqueException("Erro ao realizar baixa no estoque");
+        } catch (ProdutoException e) {
+            System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
+            throw new BaixaEstoqueException("Erro ao realizar baixa no estoque");
+        }
     }
 }
---
+++ src/com/java17/_8_excecoes/estoque/exceptions/BaixaEstoqueException.java
@@ -0,0 +1,8 @@
+package com.java17._8_excecoes.estoque.exceptions;
+
+public class BaixaEstoqueException extends Exception{
+
+    public BaixaEstoqueException(String message) {
+        super(message);
+    }
+}
