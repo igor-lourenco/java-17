48324ea5373059691774ccb782ca6b44eb953855
igor-lourenco <igor.lourencosantos@gmail.com>
2025-07-24 09:58:50 -0300

    Capturando exceções com multi-catch

--- src/com/java17/_8_excecoes/estoque/Principal.java
+++ src/com/java17/_8_excecoes/estoque/Principal.java
@@ -42,10 +42,7 @@ public class Principal {
         try {
             produto.retirarEstoque(quantidade);
             System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n",quantidade, produto.getQuantidadeEstoque());
-        } catch (IllegalArgumentException e){
-            System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
-            throw new BaixaEstoqueException("Erro ao realizar baixa no estoque", e);
-        } catch (ProdutoException e) {
+        } catch (IllegalArgumentException | ProdutoException e){
             System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
             throw new BaixaEstoqueException("Erro ao realizar baixa no estoque", e);
         }
