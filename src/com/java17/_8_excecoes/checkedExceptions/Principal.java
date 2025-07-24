718b81950f0b3ca4a063fb59ed870a9c655049f9
igor-lourenco <igor.lourencosantos@gmail.com>
2025-07-24 10:21:53 -0300

    Usando a cláusula finally

--- src/com/java17/_8_excecoes/checkedExceptions/Principal.java
+++ src/com/java17/_8_excecoes/checkedExceptions/Principal.java
@@ -1,5 +1,6 @@
 package com.java17._8_excecoes.checkedExceptions;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,13 +9,28 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        Path arquivo = Path.of("C:\\temp\\especialista-java\\java-17\\abc\\teste.txt");
-
+        Path arquivo = Path.of("C:\\temp\\especialista-java\\abc\\java-17\\teste.txt");
+        BufferedReader reader = null;
         try {
-            Files.createFile(arquivo);
+
+            reader = Files.newBufferedReader(arquivo);
+
+            System.out.println(reader.readLine());
+
+
         } catch (IOException e) {
             System.out.println("Erro ao criar arquivo: " + e.getMessage());
-            e.printStackTrace();
+//            e.printStackTrace();
+        } finally {
+            try {
+                System.out.println("DEBUG: Fechando o leitor de arquivo!");
+                reader.close();
+            } catch (IOException e) {
+                System.out.println("Erro ao fechar o leitor de arquivo: " + e.getMessage());
+            } catch (NullPointerException e) {
+                System.out.println("Erro ao fechar o leitor de arquivo: " + e.getMessage());
+            }
+
         }
 
     }
