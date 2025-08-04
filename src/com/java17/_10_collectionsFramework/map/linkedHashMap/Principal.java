aea2b83a810d8420691479b293f73882107b5dc3
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-04 10:28:31 -0300

    Usando mapas dos tipo LinkedHashMap

---
+++ src/com/java17/_10_collectionsFramework/map/linkedHashMap/Principal.java
@@ -0,0 +1,85 @@
+package com.java17._10_collectionsFramework.map.linkedHashMap;
+
+import com.java17._10_collectionsFramework.map.linkedHashMap.modelo.Carro;
+import com.java17._10_collectionsFramework.map.linkedHashMap.modelo.Proprietario;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Carro bmw = new Carro("ALG1A23", "BMW X3");
+        Carro honda = new Carro("WOR9K87", "Honda HR-V");
+        Carro ford = new Carro("JAV5A67", "Ford Ka");
+        Carro porshe = new Carro("EJA0V44", "Porshe 911");
+
+        Proprietario maria = new Proprietario("Maria", "11111111111");
+        Proprietario ana = new Proprietario("Ana", "11111111111");
+        Proprietario jose = new Proprietario("Jose", "11111111111");
+        Proprietario joao = new Proprietario("Joao", "11111111111");
+
+        // Mantém a ordem de inserção e usa mais memória que o HashMap e é um pouquinho mais lento que o HashMap
+        Map<Carro, Proprietario> proprietarios = new LinkedHashMap<>();
+        proprietarios.put(bmw, maria);
+        proprietarios.put(honda, ana);
+        proprietarios.put(ford, jose);
+        proprietarios.put(porshe, maria);
+
+
+//        System.out.println(proprietarios);
+//        System.out.printf("\nO proprietario do carro %s = %s",ford, proprietarios.get(ford));
+
+        System.out.println("Carros:");
+        Set<Carro> carros1 = proprietarios.keySet(); // lista de chaves
+        for (Carro carro : carros1) {
+            System.out.println(carro);
+        }
+
+
+        System.out.println();
+        System.out.println("Proprietarios: ");
+        Collection<Proprietario> values = proprietarios.values(); // lista dos valores
+        for (Proprietario proprietario : values) {
+            System.out.println(proprietario);
+        }
+
+
+        System.out.println();
+        System.out.println("Percorrendo o Map:");
+        Set<Carro> carros2 = proprietarios.keySet();
+        for (Carro carro : carros2) {
+            Proprietario prop = proprietarios.get(carro);
+            System.out.printf("%s (%s) = %s%n", carro.getPlaca(), carro.getModelo(), prop.getNome());
+        }
+
+
+        System.out.println();
+        System.out.println("Percorrendo o Map da melhor forma:");
+        Set<Map.Entry<Carro, Proprietario>> entries = proprietarios.entrySet();
+        for (Map.Entry<Carro, Proprietario> entry : entries) {
+            System.out.printf("%s (%s) = %s%n",
+                entry.getKey().getPlaca(), entry.getKey().getModelo(), entry.getValue().getNome());
+        }
+
+
+        proprietarios.put(porshe, jose);
+        System.out.println();
+        System.out.println("Alterando o valor relacionado a chave: " + porshe.getModelo());
+        for (Map.Entry<Carro, Proprietario> entry : entries) {
+            System.out.printf("%s (%s) = %s%n",
+                entry.getKey().getPlaca(), entry.getKey().getModelo(), entry.getValue().getNome());
+        }
+
+
+        proprietarios.remove(porshe);
+        System.out.println();
+        System.out.println("Removendo pela chave: " + porshe.getModelo());
+        for (Map.Entry<Carro, Proprietario> entry : entries) {
+            System.out.printf("%s (%s) = %s%n",
+                entry.getKey().getPlaca(), entry.getKey().getModelo(), entry.getValue().getNome());
+        }
+    }
+}
---
+++ src/com/java17/_10_collectionsFramework/map/linkedHashMap/modelo/Proprietario.java
@@ -0,0 +1,52 @@
+package com.java17._10_collectionsFramework.map.linkedHashMap.modelo;
+
+import java.util.Objects;
+
+public class Proprietario {
+
+    private String nome;
+    private String cpf;
+
+    public Proprietario(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Proprietario that = (Proprietario) object;
+        return Objects.equals(cpf, that.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Proprietario{");
+        sb.append("nome='").append(nome).append('\'');
+        sb.append(", cpf='").append(cpf).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
