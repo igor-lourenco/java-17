7a3cb5c954446e91521545a00fe4dded2a79527e
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-07 11:10:14 -0300

    Validando o conteudo de strings

--- src/com/java17/_12_trabalhandoComStrings/Principal.java
+++ src/com/java17/_12_trabalhandoComStrings/Principal.java
@@ -7,8 +7,57 @@ public class Principal {
         String nome1 = "Joao";
         String nome2 = new String("Joao");
 
-        System.out.println(nome1 == nome2); // compara o endereço de mémoria das Strings
-        System.out.println(nome1.equals(nome2)); // compara o conteúdo das Strings
-    }
+        System.out.println();
+        System.out.printf("Comparando endereco de memoria: %b%n", nome1 == nome2); // compara o endereço de mémoria das Strings
+        System.out.println("Comparando conteudo: " + nome1.equals(nome2)); // compara o conteúdo das Strings
 
+
+//      TODO: método equalsIgnoreCase
+        String nome3 = "Joao";
+        String nome4 = new String("JOAO");
+
+        System.out.println();
+        System.out.println(">>> equalsIgnoreCase");
+        System.out.println("Comparando maiuscula e minuscula: " + nome3.equalsIgnoreCase(nome4));
+
+
+//      TODO: método isEmpty
+        String nome5 = "";
+
+        System.out.println();
+        System.out.println(">>> isEmpty");
+        System.out.println("Comparando string vazia: " + nome5.isEmpty());
+
+//      TODO: método isBlank
+        String nome6 = "         ";
+
+        System.out.println();
+        System.out.println(">>> isBlank");
+        System.out.println("Comparando string em branco(com espacos): " + nome6.isBlank());
+
+
+//      TODO: método startsWith
+        String nome7 = "Joao";
+
+        System.out.println();
+        System.out.println(">>> startsWith");
+        System.out.println("Verifica se a string comeca com caractere especifico(J): " + nome7.startsWith("J"));
+
+//      TODO: método endsWith
+        String nome8 = "Joao";
+
+        System.out.println();
+        System.out.println(">>> endsWith");
+        System.out.println("Verifica se a string termina com caractere especifico(o): " + nome8.startsWith("o"));
+
+//      TODO: método contains
+        String nome9 = "Joao";
+
+        System.out.println();
+        System.out.println(">>> contains");
+        System.out.println("Verifica se a string contem caractere especifico(ao): " + nome9.contains("ao"));
+
+
+
+    }
 }
