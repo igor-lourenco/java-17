aeea67e6ba4790beca9a2c1a5bed1fc11b3d75ce
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-07 11:36:30 -0300

    Extraindo trechos da String com indexOf e substring

--- src/com/java17/_12_trabalhandoComStrings/Principal.java
+++ src/com/java17/_12_trabalhandoComStrings/Principal.java
@@ -58,6 +58,22 @@ public class Principal {
         System.out.println("Verifica se a string contem caractere especifico(ao): " + nome9.contains("ao"));
 
 
+//      TODO: métodos indexOf e substring
+        String nome10 = "Joao Silva Souza";
+        int index = nome10.indexOf(" ") + 1; // 4 + 1
+
+        if(index < 0) throw new RuntimeException("Nao e um nome completo");
+
+        System.out.println();
+        System.out.println(">>> indexOf, substring");
+        System.out.println("Todo o sobrenome do nome '" + nome10 + "': " + nome10.substring(index));
+
+        int end = nome10.indexOf(" ", index); // 10
+
+        if(end < 0) end = nome10.length(); // Se não tiver o " ", pega o tamanho da string
+
+        System.out.println("Apenas o primeiro sobrenome do nome '" + nome10 + "': " + nome10.substring(index, end));
+
 
     }
 }
