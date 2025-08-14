9027d6c7d9dd871e07f78b2e2b276742405659ca
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-14 15:02:00 -0300

    Calculando e comparando datas com Date

--- src/com/java17/_14_dateECalendarAsAPIsLegadas/Principal.java
+++ src/com/java17/_14_dateECalendarAsAPIsLegadas/Principal.java
@@ -11,5 +11,27 @@ public class Principal {
         Date data = new Date(System.currentTimeMillis() - dia);
 
         System.out.println("Data de ontem: " + data);
+
+//      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        Date hoje = new Date();
+        Date ontem = new Date(System.currentTimeMillis() - dia);
+
+        System.out.println();
+        System.out.println("Hoje em milissegundos: " + hoje.getTime());
+        System.out.println("Ontem em milissegundos: " + ontem.getTime());
+
+        long diferencaDeOntemParaHojeEmMilis = hoje.getTime() - ontem.getTime();
+
+        double diferencaDeOntemParaHojeEmHoras = diferencaDeOntemParaHojeEmMilis / 1000 / 60 / 60;
+
+        System.out.println("Diferenca em milissegundos: " + diferencaDeOntemParaHojeEmMilis);
+        System.out.printf("Diferenca em horas: %.2f%n", diferencaDeOntemParaHojeEmHoras);
+
+        System.out.printf("Hoje eh depois de ontem: %b%n", hoje.after(ontem));
+        System.out.printf("Hoje eh antes de ontem: %b%n", hoje.before(ontem));
+        System.out.printf("Comparacao: %d%n", hoje.compareTo(ontem));
+
     }
 }
