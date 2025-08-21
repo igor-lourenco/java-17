4da8b8dc5e62e3958ecbce0cfb9dfad9d23e78e2
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-21 11:11:46 -0300

    Comparando objetos temporais

--- src/com/java17/_15_dateTimeAPIMaisNovaEModerna/Main.java
+++ src/com/java17/_15_dateTimeAPIMaisNovaEModerna/Main.java
@@ -3,6 +3,7 @@ package com.java17._15_dateTimeAPIMaisNovaEModerna;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.chrono.ThaiBuddhistDate;
 
 public class Main {
 
@@ -33,5 +34,24 @@ public class Main {
         hora = LocalTime.from(dataEHora);
         System.out.println("Extraindo apenas a hora do LocalDateTime usando o from() do LocalTime: " + hora);
 
+        System.out.println("===========================================================================");
+
+        LocalDateTime dataDepoisDeAmanha = dataEHora.plusDays(2);
+        boolean ehMaior = dataDepoisDeAmanha.isAfter(dataEHora);
+        System.out.printf("%n1. Data [%s] é maior que a data [%s]: %b%n", dataDepoisDeAmanha, dataEHora, ehMaior);
+
+        LocalDateTime dataAntesDeOntem = dataEHora.minusDays(2);
+        boolean ehMenor = dataAntesDeOntem.isBefore(dataEHora);
+        System.out.printf("%n2. Data [%s] é menor que a data [%s]: %b%n", dataAntesDeOntem, dataEHora, ehMenor);
+
+        boolean ehIgual = dataAntesDeOntem.isEqual(dataDepoisDeAmanha);
+        System.out.printf("%n3. Data [%s] é igual que a data [%s]: %b%n", dataAntesDeOntem, dataDepoisDeAmanha, ehIgual);
+
+        ThaiBuddhistDate hojeThai = ThaiBuddhistDate.now();
+        System.out.printf("%n4. Data atual do calendário budista: %s%n", hojeThai);
+
+        boolean dataAtualEhIgualAoCalendarioBudista = data.isEqual(hojeThai);
+        System.out.printf("%n5. Data atual [%s] é igual a data atual do calendário budista [%s]: %b%n",
+            data, hojeThai, dataAtualEhIgualAoCalendarioBudista);
     }
 }
