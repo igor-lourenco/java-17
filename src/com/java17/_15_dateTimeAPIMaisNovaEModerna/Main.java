554690e7d201c4690645863e6edd08b08323337a
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-22 15:44:07 -0300

    Instanciando objetos temporais em um fuso horário específico

--- src/com/java17/_15_dateTimeAPIMaisNovaEModerna/Main.java
+++ src/com/java17/_15_dateTimeAPIMaisNovaEModerna/Main.java
@@ -3,11 +3,18 @@ package com.java17._15_dateTimeAPIMaisNovaEModerna;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.chrono.ThaiBuddhistDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        DateTimeFormatter formatarData = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        DateTimeFormatter formatarDataEHora = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.SSSSSSSSS");
+        DateTimeFormatter formatarHora = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS");
 
         LocalDate data = LocalDate.of(1981, 9, 13);
         LocalTime hora = LocalTime.of(23, 54, 10);
@@ -53,5 +60,39 @@ public class Main {
         boolean dataAtualEhIgualAoCalendarioBudista = data.isEqual(hojeThai);
         System.out.printf("%n5. Data atual [%s] é igual a data atual do calendário budista [%s]: %b%n",
             data, hojeThai, dataAtualEhIgualAoCalendarioBudista);
+
+        System.out.println("===========================================================================");
+        System.out.println("Obs: LocalDate LocalTime e LocalDateTime não armazenam o fuso horário");
+
+        ZoneId fusoSaoPaulo = ZoneId.of("America/Sao_Paulo");
+        ZoneId fusoLosAngeles = ZoneId.of("America/Los_Angeles");
+        ZoneId fusoJapao = ZoneId.of("Japan");
+
+        LocalDate dataSaoPaulo = LocalDate.now(fusoSaoPaulo);
+        LocalDate dataLosAngeles = LocalDate.now(fusoLosAngeles);
+        LocalDate dataJapao = LocalDate.now(fusoJapao);
+
+        System.out.println("\n1. Data atual no fuso horário de São Paulo: " + formatarData.format(dataSaoPaulo));
+        System.out.println("2. Data atual no fuso horário de Los Angeles: " + formatarData.format(dataLosAngeles));
+        System.out.println("3. Data atual no fuso horário de Japão: " + formatarData.format(dataJapao));
+
+
+        LocalDateTime dataHoraSaoPaulo = LocalDateTime.now(fusoSaoPaulo);
+        LocalDateTime dataHoraLosAngeles = LocalDateTime.now(fusoLosAngeles);
+        LocalDateTime dataHoraJapao = LocalDateTime.now(fusoJapao);
+
+        System.out.println("\n4. Data e hora atual no fuso horário de São Paulo: " + formatarDataEHora.format(dataHoraSaoPaulo));
+        System.out.println("5. Data e hora atual no fuso horário de Los Angeles: " + formatarDataEHora.format(dataHoraLosAngeles));
+        System.out.println("6. Data e hora atual no fuso horário de Japão: " + formatarDataEHora.format(dataHoraJapao));
+
+
+        LocalTime horaSaoPaulo = LocalTime.now(fusoSaoPaulo);
+        LocalTime horaLosAngeles = LocalTime.now(fusoLosAngeles);
+        LocalTime horaJapao = LocalTime.now(fusoJapao);
+
+        System.out.println("\n7. Hora atual no fuso horário de São Paulo: " + formatarHora.format(horaSaoPaulo));
+        System.out.println("8. Hora atual no fuso horário de Los Angeles: " + formatarHora.format(horaLosAngeles));
+        System.out.println("9. Hora atual no fuso horário de Japão: " + formatarHora.format(horaJapao));
+
     }
 }
