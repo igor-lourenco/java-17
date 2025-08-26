37f337b067dd61962fda8958c1eb3959aed08d14
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-26 11:23:58 -0300

    Usando a interface @FunctionalInterface

--- src/com/java17/_17_expressoesLambdaEMethodReference/implementandoExpressoesLambda/Filtro.java
+++ src/com/java17/_17_expressoesLambdaEMethodReference/implementandoExpressoesLambda/Filtro.java
@@ -1,6 +1,9 @@
 package com.java17._17_expressoesLambdaEMethodReference.implementandoExpressoesLambda;
 
+@FunctionalInterface // anotação informativa
 public interface Filtro<T> {
 
     boolean avaliar(T objeto);
+
+//    void testar();
 }
