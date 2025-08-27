49683bf04fc915378f092087be56d85c46aa1986
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-27 10:08:21 -0300

    Interface funcional Consumer, iterando em coleções com forEach

---
+++ src/com/java17/_17_expressoesLambdaEMethodReference/interface_Consumer/IterandoEmColecoesComForEach.java
@@ -0,0 +1,30 @@
+package com.java17._17_expressoesLambdaEMethodReference.interface_Consumer;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class IterandoEmColecoesComForEach {
+
+    public static void main(String[] args) {
+
+        var produtos = new ArrayList<Produto>();
+        produtos.add(new Produto("Sabão", new BigDecimal("9.9"), 11));
+        produtos.add(new Produto("Leite", new BigDecimal("3.4"), 22));
+        produtos.add(new Produto("Macarrão", new BigDecimal("9.3"), 0));
+        produtos.add(new Produto("Cerveja", new BigDecimal("5.8"), 14));
+        produtos.add(new Produto("Arroz", new BigDecimal("15.9"), 0));
+        produtos.add(new Produto("Chocolate", new BigDecimal("25.1"), 10, Produto.Status.INATIVO));
+
+//        Sem usar Expressão Lambda
+//        for (Produto produto : produtos) {
+//            System.out.println(produto);
+//        }
+
+        Consumer<Produto> zerandoEstoque = produto -> produto.setQuantidade(0);
+        Consumer<Produto> imprimir = produto -> System.out.println(produto);
+
+        System.out.println("\nZerando a quantidade de estoque e depois imprimir:");
+        produtos.forEach(zerandoEstoque.andThen(imprimir));
+    }
+}
---
+++ src/com/java17/_17_expressoesLambdaEMethodReference/interface_Consumer/Produto.java
@@ -0,0 +1,83 @@
+package com.java17._17_expressoesLambdaEMethodReference.interface_Consumer;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Produto {
+
+    public enum Status{
+        ATIVO, INATIVO
+    }
+
+    private String nome;
+    private BigDecimal valor;
+    private int quantidade;
+    private Status status = Status.ATIVO;
+
+    public Produto(String nome, BigDecimal valor, int quantidade) {
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public Produto(String nome, BigDecimal valor, int quantidade, Status status) {
+        this(nome, valor, quantidade);
+        this.status = status;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Produto produto = (Produto) object;
+        return Objects.equals(nome, produto.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Produto{");
+        sb.append("nome='").append(nome).append('\'');
+        sb.append(", valor=").append(valor);
+        sb.append(", quantidade=").append(quantidade);
+        sb.append(", status=").append(status);
+        sb.append('}');
+        return sb.toString();
+    }
+}
