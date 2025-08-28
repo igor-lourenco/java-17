242992578c022d6aacd57f0729e5b173092734fc
igor-lourenco <igor.lourencosantos@gmail.com>
2025-08-28 15:38:11 -0300

    Obtendo valor e lançando exceção com orElseThrow

--- src/com/java17/_18_optional/ciaaerea/ServicoDeBagagem.java
+++ src/com/java17/_18_optional/ciaaerea/ServicoDeBagagem.java
@@ -3,7 +3,6 @@ package com.java17._18_optional.ciaaerea;
 import com.java17._18_optional.ciaaerea.exceptions.ReservaNaoEncontradaException;
 
 import java.util.Objects;
-import java.util.Optional;
 
 public class ServicoDeBagagem {
 
@@ -24,13 +23,9 @@ public class ServicoDeBagagem {
 
 //      TODO: aqui poderia ter outras regras de negócio
 
-        Optional<Reserva> reservaOptional = servicoDeReserva.buscar(codigoReserva);
-
-        if (reservaOptional.isPresent()) {
-            reservaOptional.get().adicionarBagagens(quantidadeBagagens);
-        } else {
-            throw new ReservaNaoEncontradaException("Reserva não encontrada: " + codigoReserva);
-        }
+        servicoDeReserva.buscar(codigoReserva)
+            .orElseThrow(() -> new ReservaNaoEncontradaException("Reserva não encontrada: " + codigoReserva))
+            .adicionarBagagens(quantidadeBagagens);
 
 
     }
