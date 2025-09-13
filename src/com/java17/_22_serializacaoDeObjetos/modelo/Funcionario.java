41754c20b1ec6ee573e5c4e6e0e892105603dbb5
igor-lourenco <igor.lourencosantos@gmail.com>
2025-09-13 16:35:11 -0300

    Entendendo e controlando serialVersionUID

--- src/com/java17/_22_serializacaoDeObjetos/modelo/Funcionario.java
+++ src/com/java17/_22_serializacaoDeObjetos/modelo/Funcionario.java
@@ -1,13 +1,20 @@
 package com.java17._22_serializacaoDeObjetos.modelo;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Funcionario implements Serializable {
 
+    @Serial // controlando o serialVersionUID para versionar a estrutura da classe
+    private static final long serialVersionUID = -1956764967379042128L;
+
     private String nome;
     private LocalDate dataNascimento;
+    private Integer codigo;
+
     private transient BigDecimal salario;  // transient ignora propriedade para não ser serializado
 
     public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario) {
@@ -45,8 +52,22 @@ public class Funcionario implements Serializable {
         final StringBuilder sb = new StringBuilder("Funcionario{");
         sb.append("nome='").append(nome).append('\'');
         sb.append(", dataNascimento=").append(dataNascimento);
+        sb.append(", codigo=").append(codigo);
         sb.append(", salario=").append(salario);
         sb.append('}');
         return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Funcionario that = (Funcionario) object;
+        return Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
     }
 }
