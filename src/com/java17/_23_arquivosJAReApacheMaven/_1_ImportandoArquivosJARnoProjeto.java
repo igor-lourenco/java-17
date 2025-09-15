a2eb142cec8a5f5f7d10d3a84b92bb4512311338
igor-lourenco <igor.lourencosantos@gmail.com>
2025-09-15 10:33:32 -0300

    Importando arquivos JAR no projeto

---
+++ src/com/java17/_23_arquivosJAReApacheMaven/_1_ImportandoArquivosJARnoProjeto.java
@@ -0,0 +1,27 @@
+package com.java17._23_arquivosJAReApacheMaven;
+
+import com.works.crm.api.CadastroCliente;
+import com.works.crm.api.Cliente;
+
+import java.util.List;
+
+public class _1_ImportandoArquivosJARnoProjeto {
+
+    public static void main(String[] args) {
+
+        CadastroCliente cadastroCliente = new CadastroCliente();
+
+        cadastroCliente.adicionar(new Cliente("João da Silva", 30));
+        cadastroCliente.adicionar(new Cliente("Maria Conceição", 90));
+        cadastroCliente.adicionar(new Cliente("Sebastião das Couves", 50));
+        cadastroCliente.adicionar(new Cliente("Joaquina de Jesus", 45));
+        cadastroCliente.adicionar(new Cliente("Josefina Amaral", 25));
+
+        List<Cliente> clientes = cadastroCliente.consultar(c -> c.getIdade() > 40);
+
+        for (Cliente cliente : clientes) {
+            System.out.printf("%s - %d%n", cliente.getNome(), cliente.getIdade());
+        }
+
+    }
+}
